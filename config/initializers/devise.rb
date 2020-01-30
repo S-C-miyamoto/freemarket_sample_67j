ab544c600d6035af0246e9c872fc2500257e50ee
sakutakazuho <59197668+sakutakazuho@users.noreply.github.com>
2020-01-30 19:47:35 +0900

    devise.rbの修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,13 +6,13 @@ Devise.setup do |config|
   require 'devise/orm/active_record'
 
   config.omniauth :google_oauth2,
-                  ENV['GOOGLE_APP_ID']
-                  ENV['GOOGLE_APP_SECRET']
+                  ENV['GOOGLE_APP_ID'],
+                  ENV['GOOGLE_APP_SECRET'],
                   name: :google
 
   config.omniauth :facebook,
-                  ENV['FACEBOOK_ID']
-                  ENV['FACEBOOK_SECRET']
+                  ENV['FACEBOOK_ID'],
+                  ENV['FACEBOOK_SECRET'],
                   name: :facebook
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
