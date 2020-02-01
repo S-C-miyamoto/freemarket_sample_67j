dfa80f520ebfb8a7c296b60e271a198a613ea123
mizo-0363 <gugu.net1977@gmail.com>
2020-02-01 18:46:56 +0900

    Signup Server side

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,89 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :session_and_valid_for_new_address, only: [:new_address]
+  before_action :session_and_valid_for_create_address, only: [:create_address]
+  # before_action :user_params, only: [:create_address]
+  # before_action :configure_account_update_params, only: [:update]
+
+  # GET /resource/sign_up
+  def new
+    @user = User.new
+  end
+
+  def new_address
+    @address = @user.build_address
+  end
+
+  # POST /resource
+  def create_address
+    @address = Address.new(address_params)
+    @user.build_address(@address.attributes)
+    @user.save
+    redirect_to signup_path
+  end
+
+  def session_and_valid_for_new_address
+    session[:nickname] = user_params[:nickname]
+    session[:age] = user_params[:age]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:last_name] = user_params[:last_name]
+    session[:first_name] = user_params[:first_name]
+    session[:last_name_kana] = user_params[:last_name_kana]
+    session[:first_name_kana] = user_params[:first_name_kana]
+    session[:birth_year] = user_params[:birth_year]
+    session[:birth_month] = user_params[:birth_month]
+    session[:birth_day] = user_params[:birth_day]
+    @user = User.new(
+      nickname: session[:nickname],
+      age: session[:age],
+      email: session[:email],
+      password: session[:password],
+      last_name: session[:last_name],
+      first_name: session[:first_name],
+      last_name_kana: session[:last_name_kana],
+      first_name_kana: session[:first_name_kana],
+      birth_year: session[:birth_year],
+      birth_month: session[:birth_month],
+      birth_day: session[:birth_day]
+    )
+  end
+
+  def session_and_valid_for_create_address
+    @user = User.new(
+      nickname: session[:nickname],
+      age: session[:age],
+      email: session[:email],
+      password: session[:password],
+      last_name: session[:last_name],
+      first_name: session[:first_name],
+      last_name_kana: session[:last_name_kana],
+      first_name_kana: session[:first_name_kana],
+      birth_year: session[:birth_year],
+      birth_month: session[:birth_month],
+      birth_day: session[:birth_day]
+    )
+  end
+
+   protected
+
+  def address_params
+  params.require(:address).permit(:zipcode, :prefecture, :city, :detail_address, :buidling, :optional_phone_number)
+  end
+
+
+  def user_params
+    params.require(:user).permit(
+      :nickname,
+      :age,
+      :email,
+      :password,
+      :last_name,
+      :first_name,
+      :last_name_kana,
+      :first_name_kana,
+      :birth_year,
+      :birth_month,
+      :birth_day
+      )
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_155731) do
+ActiveRecord::Schema.define(version: 2020_01_30_211247) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "zipcode", null: false
+    t.string "prefecture", null: false
+    t.string "city", null: false
+    t.string "detail_address", null: false
+    t.string "building"
+    t.string "optional_phone_number"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -26,7 +39,17 @@ ActiveRecord::Schema.define(version: 2020_01_29_155731) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "nickname", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "age"
+    t.text "self_introduction"
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "birth_year", null: false
+    t.string "birth_month", null: false
+    t.string "birth_day", null: false
     t.string "users"
     t.string "name"
     t.string "password"
@@ -36,10 +59,16 @@ ActiveRecord::Schema.define(version: 2020_01_29_155731) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
 end
