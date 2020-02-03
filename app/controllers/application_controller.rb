c75aa5b6d82c42572187333b4b13ceaa815e30ac
S-C-miyamoto <raisbritannia2020@gmail.com>
2020-02-03 14:02:36 +0900

    basic01

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth, if: :production?
+  protect_from_forgery with: :exception
 
   protected
 
@@ -10,4 +12,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :age, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day])
   end
 
+  def production?
+    Rails.env.production?
+  end
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
