db80a5e1fb66d743bc32aca2ab5626b051f10279
mizo-0363 <gugu.net1977@gmail.com>
2020-02-04 19:02:35 +0900

    商品出品機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,8 @@ class Item < ApplicationRecord
 
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User", optional: true
+
+  validates :name, presence: true
+  validates :state, presence: true
+  validates :condition, presence: true
 end
