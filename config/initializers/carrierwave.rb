7294f6eb97178b12c9c54f2709ffa2a785487913
S-C-miyamoto <raisbritannia2020@gmail.com>
2020-02-12 16:19:52 +0900

    carrierwave修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
       region: 'ap-northeast-1'
     }
-    config.fog_directory  = 'deployapp67jimage'
+    config.fog_directory  = 'deploy67jimage'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/deployapp67jimage'
   end
 end
