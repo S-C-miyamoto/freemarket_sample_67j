c3e93add0bcc39cac7a74b3670090b65b8aef04b
sakutakazuho <59197668+sakutakazuho@users.noreply.github.com>
2020-02-13 14:06:35 +0900

    単体テストの実装

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Creditcard, type: :model do
+RSpec.describe Card, type: :model do
   describe '#create' do
 
     it "is invalid without a user_id" do
-      creditcard = build(:creditcard, user_id: "")
-      creditcard.valid?
-      expect(creditcard.errors[:user_id]).to include("can't be blank")
+      card = build(:card, user_id: "")
+      card.valid?
+      expect(card.errors[:user_id]).to include("can't be blank")
     end
 
     it "is invalid without a customer_id" do
-      creditcard = build(:creditcard, customer_id: "")
-      creditcard.valid?
-      expect(creditcard.errors[:customer_id]).to include("can't be blank")
+      card = build(:card, customer_id: "")
+      card.valid?
+      expect(card.errors[:customer_id]).to include("can't be blank")
     end
 
     it "is invalid without a card_id" do
-      creditcard = build(:creditcard, card_id: "")
-      creditcard.valid?
-      expect(creditcard.errors[:card_id]).to include("can't be blank")
+      card = build(:card, card_id: "")
+      card.valid?
+      expect(card.errors[:card_id]).to include("can't be blank")
     end
 
   end
