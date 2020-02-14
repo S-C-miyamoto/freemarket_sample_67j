bc97a0ac6b7208bb94fc1eed501cb66e1cc5b908
mizo-0363 <gugu.net1977@gmail.com>
2020-02-14 16:56:38 +0900

    リファクタリング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth, if: :production?
-  protect_from_forgery with: :exception
 
   protected
 
